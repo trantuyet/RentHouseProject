ed3039e1e823e966da067d59cf2c0b495824e8d7
trandau2k2 <trantuyet.fightforfreedom@gmail.com>
2021-01-23 21:47:12 +0700

    register form

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard/dashboard.component';
 import {LoginComponent} from "./user/login/login.component";
-import {ListhouseComponent} from './house/listhouse/listhouse.component'
+import {ListhouseComponent} from './house/listhouse/listhouse.component';
+import {RegisterComponent} from './user/register/register.component'
 const routes: Routes = [
 
   {
     path: 'login',
     component:LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   },
   {
     path: 'dashboard',
