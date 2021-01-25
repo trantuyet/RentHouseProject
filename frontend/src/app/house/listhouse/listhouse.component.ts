85b1d5fed76088c403bf3945b15a2f00035ec4ea
trandau2k2 <trantuyet.fightforfreedom@gmail.com>
2021-01-25 21:04:55 +0700

    image service

--- frontend/src/app/house/listhouse/listhouse.component.ts
+++ frontend/src/app/house/listhouse/listhouse.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ImageService} from 'src/app/shared/image.service';
 @Component({
   selector: 'app-listhouse',
   templateUrl: './listhouse.component.html',
   styleUrls: ['./listhouse.component.css']
 })
 export class ListhouseComponent implements OnInit {
+  imageList: any[];
+  rowIndexArray: any[];
 
-  constructor() { }
+  constructor(private service: ImageService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.service.imageDetailList.snapshotChanges().subscribe(
+      list => {
+        this.imageList = list.map(item => { return item.payload.val(); });
+        this.rowIndexArray =  Array.from(Array(Math.ceil((this.imageList.length+1) / 3)).keys());
+      }
+    );
   }
-
 }
+
